34ce2ade04ae8c4b73bcc602d1f904ef7d7009dc
Stefan <cveticstefancpl@gmail.com>
2020-06-05 15:25:49 +0200

    Create another fragment and hook up bottom bar functionality

--- app/src/main/java/com/example/bottombarandtablayoutexample/activity/MainActivity.kt
+++ app/src/main/java/com/example/bottombarandtablayoutexample/activity/MainActivity.kt
@@ -2,23 +2,36 @@ package com.example.bottombarandtablayoutexample.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.bottombarandtablayoutexample.R
+import com.example.bottombarandtablayoutexample.fragment.HeterogenousContentFragment
 import com.example.bottombarandtablayoutexample.fragment.HomogenousContentFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         supportActionBar?.hide()
 
-        initFragment()
+        setUpBottomBar()
     }
 
-    private fun initFragment() {
+    private fun setUpBottomBar() {
+        bottomBar.onItemSelected = { position ->
+            when (position) {
+                0 -> displayFragment(HomogenousContentFragment())
+                1 -> displayFragment(HeterogenousContentFragment())
+            }
+        }
+
+        bottomBar.onItemSelected?.invoke(0)
+    }
+
+    private fun displayFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragmentContainerView, HomogenousContentFragment())
+        transaction.replace(R.id.fragmentContainerView, fragment)
         transaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/bottombarandtablayoutexample/fragment/HeterogenousContentFragment.kt
@@ -0,0 +1,7 @@
+package com.example.bottombarandtablayoutexample.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.bottombarandtablayoutexample.R
+
+class HeterogenousContentFragment: Fragment(R.layout.fragment_heterogenous_content) {
+}
